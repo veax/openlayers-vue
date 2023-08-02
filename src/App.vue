528e27a61419174bfad887ba5fe833b72e0e4d25
veax <borissowadim@mail.ru>
2023-08-02 21:41:33 +0200

    init ol map

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { onMounted } from "vue"
+import OSM from "ol/source/OSM"
+import TileLayer from "ol/layer/Tile"
+import { Map, View } from "ol"
+import { fromLonLat } from "ol/proj"
+
+onMounted(() => {
+  console.log("on mounted call")
+  new Map({
+    target: "map",
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+    ],
+    view: new View({
+      center: fromLonLat([0, 0]),
+      zoom: 2,
+    }),
+  })
+})
+</script>
+
+<template>
+  <div id="map"></div>
+</template>
+
+<style scoped>
+#map {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
