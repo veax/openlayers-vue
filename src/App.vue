eada826943fbd5c238c2de14b00257e18245c08a
veax <borissowadim@mail.ru>
2023-08-06 22:23:45 +0200

    init ol zoom controls

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
-import { onMounted } from "vue"
+import { ref, onMounted } from "vue"
 import OSM from "ol/source/OSM"
 import TileLayer from "ol/layer/Tile"
 import { Map, View } from "ol"
 import { fromLonLat } from "ol/proj"
+import { defaults as defaultControls } from "ol/control"
+import MapBar from "./components/MapBar.vue"
+
+// state
+let map = ref<Map | null>(null)
 
 onMounted(() => {
   console.log("on mounted call")
-  new Map({
+  map.value = new Map({
     target: "map",
     layers: [
       new TileLayer({
@@ -18,17 +23,24 @@ onMounted(() => {
       center: fromLonLat([0, 0]),
       zoom: 2,
     }),
+    controls: defaultControls({
+      zoom: false,
+      attribution: false,
+      rotate: false,
+    }),
   })
 })
 </script>
 
 <template>
   <div id="map"></div>
+  <MapBar v-if="map" :map="(map as Map)" />
 </template>
 
 <style scoped>
 #map {
   width: 100vw;
   height: 100vh;
+  position: relative;
 }
 </style>
---
+++ src/components/MapBar.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { MagnifyingGlassPlusIcon } from "@heroicons/vue/20/solid"
+import { MagnifyingGlassMinusIcon } from "@heroicons/vue/20/solid"
+import { Map } from "ol"
+
+// props
+const props = defineProps<{
+  map: Map
+}>()
+
+const zoomIn = () => {
+  props.map.getView().adjustZoom(1)
+}
+
+const zoomOut = () => {
+  props.map.getView().adjustZoom(-1)
+}
+</script>
+
+<template>
+  <div id="map-bar">
+    <div class="button zoom-icon" @click="zoomIn">
+      <MagnifyingGlassPlusIcon />
+    </div>
+    <div class="button zoom-icon" @click="zoomOut">
+      <MagnifyingGlassMinusIcon />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#map-bar {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  display: flex;
+  gap: 20px;
+  padding: 50px 100px;
+  background-color: rgb(255, 255, 255, 0.5);
+  border-radius: 5px;
+}
+
+.zoom-icon {
+  width: 20px;
+  height: 20px;
+}
+
+.button {
+  border-radius: 5px;
+  background-color: white;
+  padding: 10px 40px;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  transition: all 0.3s ease 0s;
+}
+
+.button:hover {
+  background-color: #2ee59d;
+  box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+  color: white;
+}
+</style>
